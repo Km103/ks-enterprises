096732a3c6bd586591d7fd49721bd66aa3ae7e41
Km103 <mittalkanishak9@gmail.com>
2024-12-31 11:40:09 +0530

    added links in navbar

--- src/components/footer/productsfooter.jsx
+++ src/components/footer/productsfooter.jsx
@@ -6,16 +6,16 @@ export default function ProductsFooter() {
             <div className="font-bold  pb-4">Products</div>
 
             <div className="flex flex-col space-y-2 text-sm">
-                <Link href="/" className="">
+                <Link href="/products/manual-grade-stretch-film" className="">
                     Manual Grade Stretch Film
                 </Link>
-                <Link href="/" className="">
-                    Hand Grade Stretch Film
+                <Link href="/products/machine-grade-stretch-film" className="">
+                    Machine Grade Stretch Film
                 </Link>
-                <Link href="/" className="">
+                <Link href="/products/agricultural-film" className="">
                     Agricultural Film
                 </Link>
-                <Link href="/" className="">
+                <Link href="/products/cling-film" className="">
                     Cling Film
                 </Link>
             </div>
